c9dd5fe98e25fd331eee88a9f7bbdcc7cf292a53
Nandan-2025 <nandanv.v@s.amity.edu>
2025-03-25 09:46:49 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,sum;
+	printf("enter the two values;");
+	scanf("%d%d",&num1,&num2);
+	sum=num1+num2;
+	priintf("the sum is: %d\n",num1,num2,sum);
+	return 0;
+}
